ba0876a288f567acaa179dd61c90f569a3c0bd47
Ethan Liao <30430093+ethanlmaoi@users.noreply.github.com>
2018-08-21 23:46:58 -0700

    added title animation + play button

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour {
+
+    [SerializeField] GameObject intro_scene;
+    [SerializeField] GameObject cambrio_title;
+    [SerializeField] GameObject play_button;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject.Instantiate(intro_scene);
+        StartCoroutine(showTitle());
+        StartCoroutine(showPlayButton());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator showTitle()
+    {
+        yield return new WaitForSeconds(4);
+        GameObject.Instantiate(cambrio_title);
+    }
+
+    IEnumerator showPlayButton()
+    {
+        yield return new WaitForSeconds(4);
+        GameObject.Instantiate(play_button);
+    }
+}
